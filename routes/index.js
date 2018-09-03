8ca7df0cbfa83c0f19df0743315583f60f9f7f17
Georg <georg.hulla@gmail.com>
2018-09-03 20:01:20 +0200

    somechanges

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Blank Web Site', pageName: 'index'});
+  res.render('index', { title: 'myfirstwebsite', pageName: 'index'});
 });
 
 module.exports = router;
